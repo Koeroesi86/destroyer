cfd894ee1e84680870552eb34c38cb6495516b73
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-16 14:00:59 +0100

    Travis magic attempt

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,7 @@
 
 cd dist
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
+curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file @./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
 echo "Uploaded latest.yml"
 find ./ -name 'eMusic Setup*.exe' -type f -exec curl -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \;
 echo "Uploaded package"
