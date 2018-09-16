a8d9c3c3840062ab066530c604c5d7f6111854d2
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-16 15:27:48 +0100

    Simplified linux targets

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,8 +2,7 @@
 
 cd dist
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-find ./ -name 'eMusic Setup*' -type f -exec echo {}
-find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \;
+find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \; -exec echo {} \;
 echo "Uploaded package"
-curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
+curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
 echo "Uploaded latest.yml"
