3530b56f1be0492d55392f2168d9220de3307401
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-16 13:21:13 +0100

    Removed key from upload

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,7 @@
 
 cd dist
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} -T ./latest.yml --key /tmp/sftp_rsa sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
+curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} -T ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
 echo "Uploaded latest.yml"
 find ./ -name 'eMusic Setup*.exe' -type f -exec curl -u ${SFTP_USER}:${SFTP_PASSWORD} -T {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME} \;
 echo "Uploaded package"
