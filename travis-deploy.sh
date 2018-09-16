27544a6f687e8d036df7d4964f7968acf1c4a83e
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-16 14:43:00 +0100

    Debug linux build targets

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,8 @@
 
 cd dist
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-find ./ -name 'eMusic Setup*.exe' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \;
+find ./ -name 'eMusic Setup*' -type f -exec echo {}
+find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \;
 echo "Uploaded package"
 curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail --upload-file ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
 echo "Uploaded latest.yml"
