3925929ae66d99d8f7ffb74f286140ad7acf18c7
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-16 15:43:24 +0100

    Fix illegal formatting

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,7 @@
 
 cd dist
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \; -exec echo {} \;
+find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F "$1" "sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/$1" - {} \; -exec echo $1 - {} \;
 echo "Uploaded package"
 curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
 echo "Uploaded latest.yml"
