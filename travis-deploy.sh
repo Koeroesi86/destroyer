8d2e1751dfb6028bc520d170f07a5732f7dc1e7f
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-17 22:10:47 +0100

    Debug file uploads

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 cd dist
+ls -la
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F "$1" "sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/$1" - {} \; -exec echo $1 - {} \;
+find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T "$1" "sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/$1" - {} \; -exec echo $1 - {} \;
 echo "Uploaded package"
-curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -F ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
+curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
 echo "Uploaded latest.yml"
