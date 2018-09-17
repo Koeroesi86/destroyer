cf54a8c1bd2d7675579cada72383a505d225dd82
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-17 23:05:05 +0100

    Latest mac yml upload

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,7 +3,7 @@
 cd dist
 ls -la
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-find ./ -name 'eMusic Setup*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T "$1" "sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/$1" - {} \; -exec echo $1 - {} \;
+find ./ -name 'eMusic-*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T "$1" "sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/$1" - {} \; -exec echo $1 - {} \;
 echo "Uploaded package"
-#curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T ./latest.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
-#echo "Uploaded latest.yml"
+curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T ./latest-mac.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
+echo "Uploaded latest.yml"
