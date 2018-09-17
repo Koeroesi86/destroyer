807d8567a57c41d21a1bd1d448773aef25dfd7db
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-17 23:40:50 +0100

    Travis node deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,36 @@
+const Client = require('ssh2-sftp-client')
+const fs = require('fs')
+const path = require('path')
+
+let sftp = new Client()
+const distPath = path.resolve(__dirname, '../dist')
+
+const {
+  SFTP_HOST,
+  SFTP_PATH,
+  SFTP_USER,
+  SFTP_PASSWORD,
+  TRAVIS_OS_NAME
+} = process.env
+
+const foundFiles = fs.readdirSync(distPath).reduce((prev, file) => {
+  const filePath = path.join(distPath, file)
+  const stats = fs.statSync(filePath)
+  if (stats.isSymbolicLink()) return prev
+
+  if (stats.isFile()) {
+    console.log('Uploading', filePath)
+    prev.push(filePath)
+  }
+  return prev
+}, [])
+
+sftp.connect({
+  host: SFTP_HOST,
+  port: '22',
+  username: SFTP_USER,
+  password: SFTP_PASSWORD
+}).then(() => {
+  const f = foundFiles.map(localFilePath => sftp.put(localFilePath, `${SFTP_PATH}/${TRAVIS_OS_NAME}`))
+  return Promise.all(f)
+}).then(() => sftp.list('/pathname'))
--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,7 +3,8 @@
 cd dist
 ls -la
 echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
-find ./ -name 'eMusic-*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T "$1" "sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/$1" - {} \; -exec echo $1 - {} \;
+find ./ -name 'eMusic-*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \; -exec echo {} \;
 echo "Uploaded package"
-curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T ./latest-mac.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest.yml
-echo "Uploaded latest.yml"
+find ./ -name 'latest*.yml' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \; -exec echo {} \;
+#curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T ./latest-mac.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest-mac.yml
+#echo "Uploaded latest-mac.yml"
