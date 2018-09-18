84fe97c59df32d87e60a7b2e8ff637e8966309cc
Krisztian Korosi <koeroesi86@gmail.com>
2018-09-18 23:46:18 +0100

    Disabled upload

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,8 +4,8 @@ cd dist
 ls -la
 #echo uploading to sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}
 #find ./ -name 'eMusic-*' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \; -exec echo {} \;
-rsync -ue "ssh -o StrictHostKeyChecking=no" ./eMusic-* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}/${TRAVIS_OS_NAME}/
-echo "Uploaded package"
+#rsync -ue "ssh -o StrictHostKeyChecking=no" ./eMusic-* ${SFTP_USER}@${SFTP_HOST}:${SFTP_PATH}/${TRAVIS_OS_NAME}/
+#echo "Uploaded package"
 #find ./ -name 'latest*.yml' -type f -exec curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T {} sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/{} \; -exec echo {} \;
 #curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} --verbose --fail -T ./latest-mac.yml sftp://${SFTP_HOST}${SFTP_PATH}/${TRAVIS_OS_NAME}/latest-mac.yml
 #echo "Uploaded latest-mac.yml"
